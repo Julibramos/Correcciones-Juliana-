85a22a594c5d5a65fb841fb5600c7f8cdf34e3a5
Julibramos <julianabularamos@gmail.com>
2024-06-22 16:55:49 -0500

    ejercicio 8- resuelto

--- ejercicio8.py
+++ ejercicio8.py
@@ -1,8 +1,6 @@
-
-8. ```python
-    numeros = [1, 2, 3, 4, 5]
-    for num in numeros:
-        if num % 2 == 0:
-            print(num, "es par")
-        else:
-            print(num, "es impar")
+numeros = [1, 2, 3, 4, 5]
+for num in numeros:
+ if num % 2 == 0:
+     print(num, "es par")
+else:
+     print(num, "es impar")
